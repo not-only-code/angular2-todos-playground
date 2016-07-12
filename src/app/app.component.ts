3f2a1cc25ad62fcb4f72acda5a9c7ac99675cf41
Carlos Sanz Garcia <studiocam6@yahoo.es>
2016-07-12 23:45:57 +0200

    'App' component isert directives '<app-new-todo>' and '<app-todo-list>', and inject both 'todoService' instance.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewTodoComponent } from './new-todo/';
+import { TodoListComponent } from './todo-list/';
+import { TodosService } from './todos.service';
+import { TodoModel } from './todo.model';
+
 
 @Component({
   moduleId: module.id,
   selector: 'main-app',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [NewTodoComponent]
+  directives: [NewTodoComponent, TodoListComponent],
+  providers: [TodosService]
 })
-export class AppComponent {
-  clearCompleted = function() {
-    console.log('clearCompleted');
-  };
+export class AppComponent implements OnInit {
+
+  constructor(private todosService:TodosService) {}
+
+  ngOnInit() {}
+
+  clearCompleted = this.todosService.clearCompleted;
 }
